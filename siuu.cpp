57e3c45647afef7228bfea1b580d89c36f97e608
AreebIdrees <141308164+AreebIdrees@users.noreply.github.com>
2023-09-12 16:03:56 +0530

    Add files via upload

---
+++ siuu.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int var1 =4;
+    int var2 =6
+   cout<<"addition"<<var1+var2<<end;
+   cout<<"multiplication"<<var1*var2<<end;
+   cout<<"division"<<var1/var2<<end;
+   cout<<"modulus"<<var1%var2<<end;
+   
+   // left shift and right
+
+ cout<<"bitwise operator"<<end;
+ cout<<"left shift"<<(var1<<var2)<<end;
+ cout<<"right shift"<<(var1>>var2)<<end;
+ cout<<"and"<<(var1&var2)<<end;
+ cout<<"or:"<<(var1|var2)<<end;
+
+
+ 
+
+   
+   
